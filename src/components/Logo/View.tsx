fd387c12f549b45d2014ecf24ba23fbef25a3f0c
elenacarrasco <elenacfnb@gmail.com>
2021-04-27 19:17:07 +0200

    UI - Create Logo Component

---
+++ src/components/Logo/View.tsx
@@ -0,0 +1,17 @@
+import {FunctionComponent} from "react";
+import {Link} from "react-router-dom";
+
+import { LogoProps } from "./View.Types";
+import styles from "../../pages/components/Navbar/View.module.css";
+import logo from "../../assets/images/logo-dark.png";
+
+const Logo: FunctionComponent<LogoProps> = ({ onClick }) => {
+
+    return(
+        <Link to='/' onClick={onClick}>
+            <img className={styles.logo} src={logo} alt="Expedición Norte"/>
+        </Link>
+    )
+}
+
+export default Logo
