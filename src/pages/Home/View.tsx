206e05fd0eeda4a8638cd8b68947c452dee87687
elenacarrasco <elenacfnb@gmail.com>
2021-04-28 15:24:16 +0200

    Views - Update Home view

--- src/pages/Home/View.tsx
+++ src/pages/Home/View.tsx
@@ -1,14 +1,17 @@
-import { Component } from 'react'
+import { BodyColor, Container, Title } from '../../assets/styles/utils/utils'
 
-class HomeView extends Component { 
+import PostList from "../components/PostList/View";
 
-    render() {
-        return (
-            <h1>
-                Home
-            </h1>
-        )
-    }
+const HomeView = () => {
+
+    return (
+        <BodyColor bgColor="#f2f4f6">
+            <Container paddingTop={30} paddingBottom={30}>
+                <Title textAlign="center">Últimos post publicados</Title>
+                <PostList layout="mosaic" orderBy="-date" order="desc" onlyPublic={true} />
+            </Container>
+        </BodyColor>
+    )
 }
 
 export default HomeView
