73c44b68cb1481a0570bf0c372543b086d2e7698
elenacarrasco <elenacfnb@gmail.com>
2021-04-29 19:03:05 +0200

    Views - Create New post view

--- src/pages/Posts/new_post/View.tsx
+++ src/pages/Posts/new_post/View.tsx
@@ -1,12 +1,17 @@
 import { Component } from 'react'
 
+import { Container, Title } from '../../../assets/styles/utils/utils'
+import PostForm from '../../components/PostForm/View'
+
 class NewPostView extends Component { 
 
     render() {
         return (
-            <h1>
-                New post
-            </h1>
+            <Container paddingTop={30} paddingBottom={30}>
+                <Title size="big">Crear nuevo post</Title>
+                <p>Comparte con nosotros todos los planes interesantes que conozcas para realizar por Asturias y así ayudarás a que otras personas también puedan disfrutarlo.</p>
+                <PostForm />
+            </Container>
         )
     }
 }
