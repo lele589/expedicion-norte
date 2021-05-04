10ff18f5cdce9d91c061176646c0d3575e788562
elenacarrasco <elenacfnb@gmail.com>
2021-05-04 16:59:43 +0200

    Redux - Add redux in index file

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './store/reducer';
+
+// @ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers());
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+         <App />
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
